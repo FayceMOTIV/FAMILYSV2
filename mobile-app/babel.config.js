6ef97e7b0f93f430276f8f62e11ac7537f30d25b
FayceMOTIV <smmafk01@gmail.com>
2025-11-22 13:40:33 +0100

    Update babel.config.js

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -4,7 +4,7 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'nativewind/babel',
-      require.resolve('expo-router/babel')
-    ]
+      'react-native-reanimated/plugin', // PAS DE TABLEAU NI D'OPTIONS POUR ÉVITER LE CONFLIT
+    ],
   };
 };
